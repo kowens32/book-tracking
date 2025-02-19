2ddcbad601cdafb805187304a710272b59b87a35
Kimberly <kowens4@elon.edu>
2025-02-18 21:44:34 -0500

    added lambda

--- bin/book-tracking.ts
+++ bin/book-tracking.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BookTrackingStack } from '../lib/book-tracking-stack';
+import { BookLibraryStack } from '../lib/book-library-stack';
 
 const app = new cdk.App();
-new BookTrackingStack(app, 'BookTrackingStack', {
+new BookLibraryStack(app, 'BookLibraryStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new BookTrackingStack(app, 'BookTrackingStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '597088055870', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
