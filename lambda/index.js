d817ad127c97ff44fb68c4d86dd09e58291b02eb
Kimberly <kowens4@elon.edu>
2025-05-26 15:51:51 -0400

    testing lambda config

--- lambda/index.js
+++ lambda/index.js
@@ -3,16 +3,14 @@ const s3 = new AWS.S3();
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    const { userId, bookId, title, email, loanerName, loanDate } = JSON.parse(event.body);
+    const {  email, title, loanerName, loanDate } = JSON.parse(event.body);
     const tableName = process.env.TABLE_NAME;
 
     const params = {
         TableName: tableName,
         Item: {
-            userId: userId,
-            bookId: bookId,
-            title: title,
             email: email,
+            title: title,
             loanerName: loanerName,
             loanDate: loanDate,
             status: 'checked-in' 
