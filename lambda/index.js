9c1e03b013fdb9f2bfda04330e8b83fb0b431b9f
Kimberly <kowens4@elon.edu>
2025-05-26 16:09:59 -0400

    udpating index

--- lambda/index.js
+++ lambda/index.js
@@ -3,35 +3,39 @@ const s3 = new AWS.S3();
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    const {  email, title, loanerName, loanDate } = JSON.parse(event.body);
-    const tableName = process.env.TABLE_NAME;
+  const { email, title, loanerName, loanDate } = JSON.parse(event.body);
+  const tableName = process.env.TABLE_NAME;
 
-    const params = {
-        TableName: tableName,
-        Item: {
-            email: email,
-            title: title,
-            loanerName: loanerName,
-            loanDate: loanDate,
-            status: 'checked-in' 
-        },
+  const params = {
+    TableName: tableName,
+    Item: {
+      email: email,
+      title: title,
+      loanerName: loanerName,
+      loanDate: loanDate,
+      status: 'checked-in',
+    },
+  };
+
+  try {
+    await dynamoDB.put(params).promise();
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+      body: JSON.stringify({ message: 'Book added successfully' }),
     };
-
-    try {
-        await dynamoDB.put(params).promise();
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Book added successfully' }),
-        };
-    } catch (error) {
-        console.error('Error writing to DynamoDB:', error);
-        return {
-            statusCode: 200,
-            headers: {
-              'Access-Control-Allow-Origin': '*', // Or 'http://localhost:3000' for stricter dev setup
-              'Access-Control-Allow-Headers': '*',
-            },
-            body: JSON.stringify({ message: 'Book added successfully' }),
-          };          
-    }
+  } catch (error) {
+    console.error('Error writing to DynamoDB:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+      },
+      body: JSON.stringify({ message: 'Failed to add book', error: error.message }),
+    };
+  }
 };
